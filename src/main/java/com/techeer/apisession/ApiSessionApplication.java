3a113e52340aa10514eff4e8c5d7216b1aab8b73
jhkim <wellsy001012@gmail.com>
2023-05-11 01:37:14 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/techeer/apisession/ApiSessionApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.apisession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSessionApplication.class, args);
+	}
+
+}
